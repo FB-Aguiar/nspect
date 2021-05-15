a1863787d3ac41799950da18b80fea90f2c6ec15
Rodolfo <rodolfo.silva.belo@gmail.com>
2021-05-15 17:24:56 -0300

    Teste emaill final

--- contactform/send.php
+++ contactform/send.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require 'vendor/autoload.php';
+require '..//vendor/autoload.php';
 
 //Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -15,30 +15,33 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'br972.hostgator.com.br';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'rodolfo.silva.belo@gmail.com';                     //SMTP username
-    $mail->Password   = 'Rdtec1219*';                               //SMTP password
+    $mail->Username   = 'noreply@inspeect.com';                     //SMTP username
+    $mail->Password   = 'noreply1804*';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('rodolfo.silva.belo@gmail.com', 'Mailer');
-    $mail->addAddress('rodolfo.silva.belo@gmail.com', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    $mail->setFrom('noreply@inspeect.com', 'Inspect Consultoria');
+    $mail->addAddress('rodolfo.silva.belo@gmail.com');     //Add a recipient
+    // $mail->addReplyTo('fabriciotcn@gmail.com');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
 
     
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = 'Novo Contato - Site Inspect';
+    $mail->Body    = 
+      "<h2>" . $_POST['name'] . "</h2>" . "</br>" . 
+      "<h4>" . $_POST['message'] . "</h4>" . "</br>" . 
+      "Email: " . $_POST['email']
+      ;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
-    echo 'Message has been sent';
+
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
